1cb7c2db865bda177e887b61eaff0deb6a5ff5e8
nuentoter <nuentoter@gmail.com>
2026-07-05 08:58:53 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,21 @@
+#include <Arduino.h>
 
+void setup()
+{
+    Serial.begin(115200);
+
+    while (!Serial)
+    {
+        delay(10);
+    }
+
+    Serial.println();
+    Serial.println("================================");
+    Serial.println("ESP32 Self-Aware Boot");
+    Serial.println("================================");
+}
+
+void loop()
+{
+    delay(1000);
+}
